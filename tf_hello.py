64d80b115849542fc60ee02e9e79074fe66da214
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-08-26 13:28:02 -0400

    Create tf_hello.py

---
+++ tf_hello.py
@@ -0,0 +1,13 @@
+#obligatory hello world program
+
+import tensorflow as tf
+
+h = tf.constant("Hello")
+w = tf.constant(" World!")
+
+hw = h + w
+
+with tf.Session() as sess:
+    ans = sess.run(hw)
+
+print(ans)
