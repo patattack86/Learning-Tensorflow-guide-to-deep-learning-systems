6565e7a525c5addf1058036a1b5e28a21b6eb18c
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-09-06 18:55:07 -0400

    Create first_regression.py

---
+++ first_regression.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+
+###----------Chapter three example----------####
+
+#simple regression
+
+#step one is to create the placeholder values for both x an dy
+x = tf.placeholder(tf.float32, shape=[None, 3])
+y_true = tf.placeholder(tf.float32, shape=None)
+
+#w represents the weights in our model and b represents the intercept
+w = tf.Variable([[0,0,0]],dtype=tf.float32,name='weights')
+b = tf.Variable(0,dtype=tf.float32,name='bias')
+
+#now we define our model which is just a simple linear regression, matmul of x and w plus bias term b
+y_pred = tf.matmul(w,tf.transpose(x)) + b
+
+#now create our loss function, first example is MSE
+los = tf.reduce_mean(tf.square(y_true-y_pred))
+
+
