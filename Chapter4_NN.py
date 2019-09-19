8e787ce7cf9f678b0aa3f4c77ce515f04f5825e6
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-09-19 14:58:41 -0400

    Update Chapter4_NN.py

--- Chapter4_NN.py
+++ Chapter4_NN.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 
 ###----------Chapter four example----------####
@@ -62,3 +63,34 @@ full1_drop = tf.nn.dropout(full_1, keep_prob=keep_prob)
 
 #output is fully connected layer with 10 units corresponding to number of labels in dataset, with minst dataset there is a possible of 10 labels.
 y_conv = full_layer(full1_drop, 10)
+
+mnist = input_data.read_data_sets(DATA_DIR, one_hot=True)
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tg.global_variables_initializer())
+
+    for i in range(STEPS):
+        batch = mnist.train.next_batch(50)
+
+        if i % 100 == 0:
+            train_accuracy = sess.run(accuracy, feed_dict={x: batch[0],
+                                                           y_: batch[1],
+                                                           keep_prob: full_1.0})
+            print("step {}, training accuracy {}".format(i, train_accuracy)
+
+        sess.run(train_step, feed_dict = {x: batch[0], y_: batch[1],
+                                          keep_prob: 0.5})
+
+        X = mnist.test.images.reshape(10, 1000, 784)
+        Y = mnist.test.labels.reshape(10, 1000, 10)
+        test_accuracy = np.mean([sess.run(accuracy,
+                                 feed_dict={x:X[i], y_:Y[i],keep_prob:1.0})
+                                 for i in range(10)])
+
+ print "test accuracy: {}".format(test_accuracy)
