4706a8d1e83deadd846e2b7630e3e13adcc74bca
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-10-31 13:39:54 -0400

    Update RNNTraining.py

--- RNNTraining.py
+++ RNNTraining.py
@@ -21,7 +21,19 @@ inputs = tf.placeholder(tf.float32, shape =[None, time_steps, element_size], nam
 
 y = tf.placeholder(tf.float32, shape=[None, num_classes], name='labels')
 
-batch_x, batch_y = mnist.train.next_batch(batch_size
+batch_x, batch_y = mnist.train.next_batch(batch_size)
 # Reshape data to get 28 sequences of 28 pixel
 batch_x = batch_x.reshape((batch_size, time_steps, element_size))
 
+def variable_summaries(var):
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev', stddev):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+#create weights and biases for inputs and hidden layers
