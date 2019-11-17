7f74f53e980462bd707224ccd654a167b86ffb6d
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-11-17 14:47:48 -0500

    Update Chapter6_advanceRNN.py

--- Chapter6_advanceRNN.py
+++ Chapter6_advanceRNN.py
@@ -17,6 +17,7 @@ digit_to_word_map[0]="PAD"
 even_sentences = []
 odd_sentences = []
 
+
 seqlens = []
 for i in range(10000):
     rand_seq_len = np.random.choice(range(3,7))
@@ -42,3 +43,52 @@ even_sentences[0:6]
 odd_sentences[0:6]
 
 seqlens[0:6]
+
+
+#splitting the sentence into indifivial words and adding each to the index map if not already present
+word2index_map = {}
+index = 0
+for sent in data:
+    for word in sent.lower().split():
+        if word not in word2index_map:
+            word2index_map[word] = index
+            index += 1
+
+#create inverse map
+index2word_map = {index: word for word, index in word2index_map.items()}
+vocabulary_size = len(index2word_map)
+
+labels = [1]*10000 + [0]*10000
+for i in range(len(labels)):
+    label=labels[i]
+    one_hot_encoding = [0]*2
+    one_hot_encoding[label] = 1
+    labels[i] = one_hot_encoding
+
+data_indices = list(range(len(data)))
+np.random.shuffle(data_indices)
+data = np.array(data)[data_indices]
+
+labels = np.array(labels)[data_indices]
+seqlens = np.array(seqlens)[data_indices]
+train_x = data[:10000]
+train_y = labels[:10000]
+train_seqlens = seqlens[:10000]
+
+test_x = data[10000:]
+test_y = labels[10000:]
+test_seqlens = seqlens[10000:]
+
+def get_sentence_batch(batch_size,data_x,data_y,data_seqlens):
+    instance_indices = list(range(len(data_x)))
+    np.random.shuffle(instance_indices)
+    batch = instance_indices[:batch_size]
+    x = [[word2index_map[word] for word in data_x[i].lower().split()] for i in batch]
+    y = [data_y[i] for i in batch]
+    seqlens = [data_seqlens[i] for i in batch]
+    return x,y,seqlens
+
+_inputs = tf.placeholder(tf.int32, shape=[batch_size,times_steps])
+_labels = tf.placeholder(tf.float32, shape=[batch_size, num_classes])
+_seqlens = tf.placeholder(tf.int32, shape=[batch_size])
+
